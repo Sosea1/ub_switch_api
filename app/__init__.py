bab437afcfd68a88c47411bf8ed22d6423c88820
Tuleshochka <sizyno.min@gmail.com>
2024-03-21 18:04:04 +0600

    project structure

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import os, config
+
+# создание экземпляра приложения
+app = Flask(__name__)
+app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
+
+# инициализирует расширения
+
+from . import views
