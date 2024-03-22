4cf192a136976d07f28fdc2158059f53b85f190c
Sosea1 <kospryshin@gmail.com>
2024-03-22 23:05:25 +0600

    Change project structure and add json parser

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,9 @@
 # pip3 install ovs --break-system-packages
 # ovs-vsctl set-manager ptcp:127.0.0.1:6640
 
+import collections
+import json
+import re
 import ovs
 import ovs.jsonrpc
 import os
@@ -47,3 +50,33 @@ def makeRequest(address: str, method: str, params: list = []):
         raise RuntimeError(f'Ошибка выполнения запроса OVS: {tabulateError(data.error)}')
 
     return data
+
+def parse_to_json(result:str):
+        
+        result_tmp = result.splitlines()
+        dliya_dublicates = []
+
+        for i in range(len(result_tmp)):
+            result_tmp[i] = result_tmp[i].lstrip()
+            result_tmp[i] = re.sub('([^\s:"]+(?=(?:[^"]+"[^"]+")*[^"]*$))', '"\g<1>"', result_tmp[i])
+            if (': ' not in result_tmp[i]):
+                result_tmp[i] = re.sub(' ', ':', result_tmp[i])
+            if (':' not in result_tmp[i]):
+                result_tmp[i] = result_tmp[i] + ":null"
+            if i != len(result_tmp) - 1:
+                result_tmp[i] = result_tmp[i] + ","
+            dliya_dublicates.append(result_tmp[i].split(":")[0]) 
+        dublicates = [item for item, count in collections.Counter(dliya_dublicates).items() if count > 1]
+        for item in dublicates:
+            indexes = [i for i,val in enumerate(result_tmp) if val.startswith(item)]
+            count = 1
+            for index in indexes:
+                tmp = result_tmp[index].split(":")
+                result_tmp[index] = tmp[0].rstrip('"') + str(count)+ '":' +tmp[1]
+                count+=1
+        result = '\n'.join(char for char in result_tmp)
+        result = "{} {}".format('{', result)
+        result = "{} {}".format(result, '}')
+        result = json.loads(result, strict=False, object_pairs_hook=collections.OrderedDict)
+        result = json.dumps(result, indent=4)
+        return result
